8503ec40414d725db135999938af4118fa7ebf1d
NICOL123 <43378862+NICOL123@users.noreply.github.com>
2019-01-10 09:13:30 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,45 @@
+//Spell #1
+
+$(".dog").click(function(){
+ $("#dog").slideUp();
+});
+
+$(".butt2").click(function(){
+ $("#dog").slideDown();
+});
+
+
+//Spell #2
+
+$(".bye").click(function(){
+  $("#girl").hide();
+});
+
+$(".hey").click(function(){
+ $("#girl").show();
+});
+
+
+//Spell #3
+
+$(".shhh").click(function(){
+ $("#boy").fadeOut();
+});
+
+$(".cry").click(function(){
+ $("#boy").fadeIn();
+});
+
+
+//spell #4
+
+$(".me").click(function(){
+ $("#me").fadeOut();
+});
+
+$(".im-back").click(function(){
+ $("#me").fadeIn();
+});
+
+
+
